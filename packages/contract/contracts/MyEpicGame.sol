d3c312d2e6520e10e767be202fa2d1b0cf261943
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-17 21:32:13 +0900

    chore: S1-L4まで

--- packages/contract/contracts/MyEpicGame.sol
+++ packages/contract/contracts/MyEpicGame.sol
@@ -1,11 +1,42 @@
 // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.18;
-
+pragma solidity ^0.8.17;
 import "hardhat/console.sol";
-
 contract MyEpicGame {
-  constructor() {
-    console.log("THIS IS MY GAME CONTRACT.");
+  // キャラクターのデータを格納する CharacterAttributes 型の 構造体（`struct`）を作成しています。
+  struct CharacterAttributes {
+    uint characterIndex;
+    string name;
+    string imageURI;
+    uint hp;
+    uint maxHp;
+    uint attackDamage;
+  }
+  // キャラクターのデフォルトデータを保持するための配列 defaultCharacters を作成します。それぞれの配列は、CharacterAttributes 型です。
+  CharacterAttributes[] defaultCharacters;
+  constructor(
+	// プレイヤーが新しく NFT キャラクターを Mint する際に、キャラクターを初期化するために渡されるデータを設定しています。これらの値は フロントエンド（js ファイル）から渡されます。
+    string[] memory characterNames,
+    string[] memory characterImageURIs,
+    uint[] memory characterHp,
+    uint[] memory characterAttackDmg
+  )
+  {
+  // ゲームで扱う全てのキャラクターをループ処理で呼び出し、それぞれのキャラクターに付与されるデフォルト値をコントラクトに保存します。
+	// 後でNFTを作成する際に使用します。
+    for(uint i = 0; i < characterNames.length; i += 1) {
+      defaultCharacters.push(CharacterAttributes({
+        characterIndex: i,
+        name: characterNames[i],
+        imageURI: characterImageURIs[i],
+        hp: characterHp[i],
+        maxHp: characterHp[i],
+        attackDamage: characterAttackDmg[i]
+      }));
+      CharacterAttributes memory character = defaultCharacters[i];
+
+	  // hardhat の console.log() では、任意の順番で最大4つのパラメータを指定できます。
+	  // 使用できるパラメータの種類: uint, string, bool, address
+      console.log("Done initializing %s w/ HP %s, img %s", character.name, character.hp, character.imageURI);
+    }
   }
 }
--- packages/contract/scripts/run.js
+++ packages/contract/scripts/run.js
@@ -1,9 +1,17 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
-  const nftGame = await gameContract.deployed();
-
-  console.log("Contract deployed to:", nftGame.address);
+  const gameContract = await gameContractFactory.deploy(
+    ["ZORO", "NAMI", "USOPP"], // キャラクターの名前
+    [
+      "https://i.imgur.com/TZEhCTX.png", // キャラクターの画像
+      "https://i.imgur.com/WVAaMPA.png",
+      "https://i.imgur.com/pCMZeiM.png",
+    ],
+    [100, 200, 300], // キャラクターのHP
+    [100, 50, 25] // キャラクターの攻撃力
+  );
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
 };
 const runMain = async () => {
   try {
