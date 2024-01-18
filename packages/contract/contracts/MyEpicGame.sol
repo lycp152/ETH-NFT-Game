15609444c650389c62ee7cffe3ba14848adf10f5
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-18 14:25:41 +0900

    feat: S1-L5まで

--- packages/contract/contracts/MyEpicGame.sol
+++ packages/contract/contracts/MyEpicGame.sol
@@ -9,6 +9,8 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "hardhat/console.sol";
 
+import "./libraries/Base64.sol";
+
 // MyEpicGameコントラクトは、NFTの標準規格であるERC721を継承します。
 contract MyEpicGame is ERC721 {
 
@@ -34,7 +36,7 @@ contract MyEpicGame is ERC721 {
   mapping(address => uint256) public nftHolders;
 
   constructor(
-	// プレイヤーが新しく NFT キャラクターを Mint する際に、キャラクターを初期化するために渡されるデータを設定しています。これらの値は フロントエンド（js ファイル）から渡されます。
+  // プレイヤーが新しく NFT キャラクターを Mint する際に、キャラクターを初期化するために渡されるデータを設定しています。これらの値は フロントエンド（js ファイル）から渡されます。
     string[] memory characterNames,
     string[] memory characterImageURIs,
     uint[] memory characterHp,
@@ -43,8 +45,8 @@ contract MyEpicGame is ERC721 {
     // 作成するNFTの名前とそのシンボルをERC721規格に渡しています。
     ERC721("OnePiece", "ONEPIECE")
   {
-	// ゲームで扱う全てのキャラクターをループ処理で呼び出し、それぞれのキャラクターに付与されるデフォルト値をコントラクトに保存します。
-	// 後でNFTを作成する際に使用します。
+  // ゲームで扱う全てのキャラクターをループ処理で呼び出し、それぞれのキャラクターに付与されるデフォルト値をコントラクトに保存します。
+  // 後でNFTを作成する際に使用します。
     for(uint i = 0; i < characterNames.length; i += 1) {
       defaultCharacters.push(CharacterAttributes({
         characterIndex: i,
@@ -93,4 +95,33 @@ contract MyEpicGame is ERC721 {
     // 次に使用する人のためにtokenIdをインクリメントします。
     _tokenIds += 1;
   }
+
+  // nftHolderAttributes を更新して、tokenURI を添付する関数を作成
+  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+    CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+    // charAttributes のデータ編集して、JSON の構造に合わせた変数に格納しています。
+    string memory strHp = Strings.toString(charAttributes.hp);
+    string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+    string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+    string memory json = Base64.encode(
+      // abi.encodePacked で文字列を結合します。
+      // OpenSeaが採用するJSONデータをフォーマットしています。
+      abi.encodePacked(
+        '{"name": "',
+        charAttributes.name,
+        ' -- NFT #: ',
+        Strings.toString(_tokenId),
+        '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+        charAttributes.imageURI,
+        '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+        strAttackDamage,'} ]}'
+      )
+    );
+    // 文字列 data:application/json;base64, と json の中身を結合して、tokenURI を作成しています。
+    string memory output = string(
+    abi.encodePacked("data:application/json;base64,", json)
+  );
+  return output;
+}
 }
