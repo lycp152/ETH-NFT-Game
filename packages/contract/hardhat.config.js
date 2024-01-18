3b1dd7648035e1b7cb1da57b508d456087aea983
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-18 14:49:11 +0900

    feat: S1-L6まで

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
